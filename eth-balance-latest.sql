29e8f4743087468c45e253669fe4bc26bd251233
meowww404 <l@miti.ai>
2023-12-29 16:19:26 +0700

    add eth-balance-latest

---
+++ eth-balance-latest.sql
@@ -0,0 +1,10 @@
+SELECT 
+    name,
+    address, 
+    blocknumber, 
+    toFloat64(balance) / 1e18 AS balance, 
+    timestamp
+FROM "balances"
+WHERE address = '0x02528938C78201f19AC4178d13CCabf4B4F180DF'
+ORDER BY blocknumber DESC
+LIMIT 1;
